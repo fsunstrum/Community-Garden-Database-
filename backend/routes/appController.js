f401b0e0d803eae558e4e0d8a10147e937945c66
Jeff <jho1211@students.cs.ubc.ca>
2024-07-23 18:07:13 -0700

    added check-db-connection endpoint

--- backend/routes/appController.js
+++ backend/routes/appController.js
@@ -3,6 +3,15 @@ const appService = require('../appService');
 
 const router = express.Router();
 
+router.get('/check-db-connection', async (req, res) => {
+    const isConnect = await appService.testOracleConnection();
+    if (isConnect) {
+        res.send('connected');
+    } else {
+        res.send('unable to connect');
+    }
+});
+
 // ----------------------------------------------------------
 // API endpoints
 // Modify or extend these routes based on your project's needs.
