9d6d5a2900455a6b954aca4c2bc56698de413687
Michelle <mlei922@students.cs.ubc.ca>
2024-07-25 12:17:08 -0700

    Donations header updated

--- frontend/src/app/donations/page.js
+++ frontend/src/app/donations/page.js
@@ -1,4 +1,5 @@
 import styles from './page.module.css';
+import Image from 'next/legacy/image';
 import DonationsTable from '@/components/DonationsTable';
 import DonationForm from '@/components/DonationForm';
 
@@ -13,17 +14,23 @@ export default async function Donations() {
     return (
         <div className={styles.container}>
             <header className={styles.header}>
+                <Image
+                    src="/donation.jpg"
+                    alt="Donation"
+                    layout="fill"
+                    objectFit="cover"
+                    className={styles.headerImage}
+                />
                 <div className={styles.headerContent}>
-                    <h1 className={styles.headerTitle}>Donations</h1>
+                    <h1 className={styles.headerTitle}>DONATIONS</h1>
                 </div>
             </header>
             <main className={styles.main}>
+                <section className={styles.table}>
+                    <DonationsTable donations={donations}></DonationsTable>
+                </section>
                 <section className={styles.form}>
                     <DonationForm />
-                    
-                </section>
-                <section className={styles.table}>
-                <DonationsTable donations={donations}></DonationsTable>
                 </section>
             </main>
         </div>
