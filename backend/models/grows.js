3ee280d32d583f29a3b6c4b2b355befac92faed9
Jeff <jho1211@students.cs.ubc.ca>
2024-07-26 12:08:12 -0700

    nested aggregation grows model function

--- backend/models/grows.js
+++ backend/models/grows.js
@@ -1,5 +1,38 @@
 const connection = require('../config/db');
 
+// Returns the gardens wtih total plants less than the average total for all gardens
+async function underAchievingGardens() {
+    let connection;
+    try {
+        connection = await getConnection();
+        const result = await connection.execute(
+            `SELECT garden_address, sumQty
+                FROM (
+                    SELECT garden_address, SUM(qty) AS sumQty 
+                    FROM GROWS 
+                    GROUP BY garden_address
+                )
+                WHERE sumQty < (
+                    SELECT AVG(qtySum) 
+                    FROM (
+                        SELECT SUM(qty) as qtySum
+                        FROM Grows 
+                        GROUP BY garden_address ))`);
+        return result.rows;
+    } catch (err) {
+        console.error('Error executing query:', err.message);
+        throw err;
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error('Error closing connection:', err.message);
+            }
+        }
+    }
+}
+
 const Grows = {
     insert: (data, callback) => {
         const sql = 'INSERT INTO Grows SET ?';
