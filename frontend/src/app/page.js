a7df6e14a80a9f2a2c427cc93c4fa745d41b5f22
Michelle <mlei922@students.cs.ubc.ca>
2024-07-29 12:58:03 -0700

    home page description updated

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -27,20 +27,19 @@ export default function Home() {
 
       <main className={styles.main}>
         <section className={styles.infoSection}>
-          <h2>What are community gardens?</h2>
+          <h2>About This Project</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lobortis tortor eu lorem dapibus mattis. 
-            Mauris vel aliquam lorem. Vestibulum vehicula facilisis felis, vel varius eros posuere et. Suspendisse volutpat nisl 
-            nec dictum congue. Maecenas risus massa, consequat et fermentum eget, finibus a ante. Phasellus in rhoncus urna. 
-            Duis id tellus vel orci mollis convallis. Aliquam a nisi quam. Morbi eu nibh ipsum. Donec luctus ipsum a lorem auctor 
-            rhoncus. Vestibulum a malesuada nulla, et laoreet arcu. Aenean arcu ex, posuere vel iaculis vitae, varius quis est.
+          This database management system will be used by a municipality to store information about various community gardens throughout 
+          the city so that they can better organize these community gardens. Each garden will be managed by different community organizations 
+          and they will record the plants that are seeded in each plot and the gardeners who are assigned to each plot. There will also be 
+          donations of tools and plants which are recorded so that the garden members will know what is available for use.
           </p>
-          <p>
+          {/* <p>
             Nullam elementum lectus id ultricies placerat. Vestibulum et ornare ante, id pharetra justo. Duis ornare erat a malesuada 
             vulputate. Pellentesque sollicitudin sodales nulla, a volutpat neque bibendum at. Donec at risus blandit, sagittis mauris 
             et, venenatis tellus. Mauris at suscipit lacus. Etiam eleifend est ligula. Suspendisse imperdiet luctus elit, id faucibus 
             lectus faucibus eu. Aliquam semper condimentum egestas.
-          </p>
+          </p> */}
         </section>
       </main>
     </div>
