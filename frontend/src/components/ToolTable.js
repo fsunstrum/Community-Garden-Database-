786b2c1c8d782fd6553138657df2cc76d7cff18c
funstrum <fsuns060@students.cs.ubc.ca>
2024-07-29 16:32:03 -0700

    Add/remove tools front end added

--- frontend/src/components/ToolTable.js
+++ frontend/src/components/ToolTable.js
@@ -5,16 +5,34 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Button, IconButton, TextField } from '@mui/material'
+import React, { useState } from 'react';
+import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
 
+export default function ToolTable({ tools }) { 
 
-export default function ToolTable({ tools }) { //???
+    const [newToolType, setNewToolType] = useState('');
+    const [newAvailability, setNewAvailability] = useState('');
 
+    const handleAddRow = () => {
+        if (newToolType && newAvailability) {
+            const newRow = [newToolType, newAvailability];
+            setTools([...tools, newRow]);
+
+            setNewToolType('');
+            setNewAvailability('');
+        }
+    };
 
 
     const tableRows = tools.map((row, idx) => (
         <TableRow key={idx}>
             {row.map((v, i) => <TableCell key={i}>{v}</TableCell>)}
-            <TableCell></TableCell>
+            <TableCell>
+                <IconButton>
+                    <DeleteForeverRoundedIcon />
+                </IconButton>
+            </TableCell>
         </TableRow>  
     ))
 
@@ -26,9 +44,34 @@ export default function ToolTable({ tools }) { //???
                         <TableCell>Tool Type</TableCell>
                         <TableCell>Availability</TableCell>
                 </TableRow>
+
                 </TableHead>
                 <TableBody>
                 {tableRows}
+                <TableRow>
+                        <TableCell>
+                            <TextField 
+                                value={newToolType} 
+                                onChange={(e) => setNewToolType(e.target.value)} 
+                                placeholder="Tool Type" 
+                            />
+                        </TableCell>
+                        <TableCell>
+                            <TextField 
+                                value={newAvailability} 
+                                disabled
+                            />
+                        </TableCell>
+                        <TableCell>
+                            <Button 
+                                variant="contained" 
+                                color="primary" 
+                                onClick={handleAddRow}
+                            >
+                                Add
+                            </Button>
+                        </TableCell>
+                    </TableRow>                
                 </TableBody>
             </Table>
         </TableContainer>
